d23a9f03a3ae8e5ce0d2c76e4e037d728e019c53
Ahmed Sbai <asbai@sqli.com>
2024-11-18 14:04:12 +0100

    init: test react native filament potential

--- App.tsx
+++ App.tsx
@@ -1,118 +1,91 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
+  Camera,
+  DefaultLight,
+  FilamentScene,
+  FilamentView,
+  Model,
+  useCameraManipulator,
+} from 'react-native-filament';
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler';
+import {Dimensions, StyleSheet, View} from 'react-native';
+import {useSharedValue} from 'react-native-worklets-core';
+import Ferrari from './Ferrari.glb';
+import HouseModel from './model.glb';
+import Workspace from './workspace.glb';
+const modelPath =
+  'https://raw.githubusercontent.com/google/filament/main/third_party/models/DamagedHelmet/DamagedHelmet.glb';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+function Scene() {
+  const cameraManipulator = useCameraManipulator({
+    orbitHomePosition: [0, 0, 8], // "Camera location"
+    targetPosition: [0, 0, 0], // "Looking at"
+    orbitSpeed: [0.003, 0.003],
+  });
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  // Pan gesture
+  const viewHeight = Dimensions.get('window').height;
+  const panGesture = Gesture.Pan()
+    .onBegin(event => {
+      const yCorrected = viewHeight - event.translationY;
+      cameraManipulator?.grabBegin(event.translationX, yCorrected, false); // false means rotation instead of translation
+    })
+    .onUpdate(event => {
+      const yCorrected = viewHeight - event.translationY;
+      cameraManipulator?.grabUpdate(event.translationX, yCorrected);
+    })
+    .maxPointers(1)
+    .onEnd(() => {
+      cameraManipulator?.grabEnd();
+    });
+
+  // Scale gesture
+  const previousScale = useSharedValue(1);
+  const scaleMultiplier = 100;
+  const pinchGesture = Gesture.Pinch()
+    .onBegin(({scale}) => {
+      previousScale.value = scale;
+    })
+    .onUpdate(({scale, focalX, focalY}) => {
+      const delta = scale - previousScale.value;
+      cameraManipulator?.scroll(focalX, focalY, -delta * scaleMultiplier);
+      previousScale.value = scale;
+    });
+  const combinedGesture = Gesture.Race(pinchGesture, panGesture);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <GestureDetector gesture={combinedGesture}>
+      <FilamentView style={styles.container}>
+        <Camera cameraManipulator={cameraManipulator} />
+        <DefaultLight />
+
+        <Model source={{uri: modelPath}} transformToUnitCube />
+        {/*<Model source={HouseModel} transformToUnitCube />*/}
+        {/*<Model source={Workspace} transformToUnitCube />*/}
+        {/*<Model source={Ferrari} transformToUnitCube />*/}
+      </FilamentView>
+    </GestureDetector>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+export default function App() {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <GestureHandlerRootView style={styles.container}>
+      <View style={styles.container}>
+        <FilamentScene>
+          <Scene />
+        </FilamentScene>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
   },
 });
-
-export default App;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  plugins: [
+    ['react-native-worklets-core/plugin', {processNestedWorklets: true}],
+    'react-native-reanimated/plugin',
+  ],
   presets: ['module:@react-native/babel-preset'],
 };
